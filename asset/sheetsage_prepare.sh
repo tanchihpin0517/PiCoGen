ad764f78fbaa8f032b826a472bb77b9a4ccbae80
Chih-Pin Tan <tanchihpin0517@gmail.com>
2024-04-29 16:42:36 +0000

    update script

--- asset/sheetsage_prepare.sh
+++ asset/sheetsage_prepare.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)
+cd ..
 
 python -m sheetsage.sheetsage.assets SHEETSAGE_V02_HANDCRAFTED ${JUKEBOX_CMD} && \
   python -m sheetsage.sheetsage.assets SHEETSAGE_V02_JUKEBOX && \

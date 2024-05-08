b5f67d3df1c8b3c0e8002d0abc3d1481c8fb6e60
Chih-Pin Tan <tanchihpin0517@gmail.com>
2024-05-08 03:08:18 +0000

    update environment setup

--- asset/sheetsage_prepare.sh
+++ asset/sheetsage_prepare.sh
@@ -6,3 +6,5 @@ cd ..
 python -m sheetsage.sheetsage.assets SHEETSAGE_V02_HANDCRAFTED ${JUKEBOX_CMD} && \
   python -m sheetsage.sheetsage.assets SHEETSAGE_V02_JUKEBOX && \
   python -m sheetsage.sheetsage.assets JUKEBOX
+
+rsync -av ~/.sheetsage/jukebox/ ~/.cache/jukebox/

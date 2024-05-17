d4038eab6bbb1bd76dfebe0f926ebb8957187886
Chih-Pin Tan <tanchihpin0517@gmail.com>
2024-05-17 04:26:16 +0000

    update the preparing script of SheetSage

--- asset/sheetsage_prepare.sh
+++ asset/sheetsage_prepare.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 cd ..
 
-python -m sheetsage.sheetsage.assets SHEETSAGE_V02_HANDCRAFTED ${JUKEBOX_CMD} && \
+python -m sheetsage.sheetsage.assets SHEETSAGE_V02_HANDCRAFTED && \
   python -m sheetsage.sheetsage.assets SHEETSAGE_V02_JUKEBOX && \
   python -m sheetsage.sheetsage.assets JUKEBOX
 

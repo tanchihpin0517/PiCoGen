33fa21a9a27a89ad11d774e3c52b201bf8309036
Chih-Pin Tan <tanchihpin0517@gmail.com>
2024-08-03 17:33:36 +0800

    minor fixes

--- demo.sh
+++ demo.sh
@@ -5,4 +5,4 @@ conda run -n picogen2 --no-capture-output pip install --upgrade yt-dlp >> /dev/n
 song_name="never_gonna_give_you_up"
 conda run -n picogen2 --no-capture-output ./infer.sh \
     --input_url 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' \
-    --output_dir ./demo_output/$song_name
+    --output_dir ./demo_output
